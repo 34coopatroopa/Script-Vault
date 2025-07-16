811fa528be1234114f99131d46d12bff86d23270
Cooper Hoy <144978421+34coopatroopa@users.noreply.github.com>
2025-07-16 10:42:17 -0500

    AUTO_AD_GRP.ps1

--- AUTO_AD_GRP.ps1
+++ AUTO_AD_GRP.ps1
@@ -1,3 +1,3 @@
-$from= Read-Host -Prompt 'Input Username to copy from  (first.last)'
-$To = Read-Host -Prompt 'Input Username to copy to   (first.last)'
+$from= Read-Host -Prompt 'Input Username to copy From'
+$To = Read-Host -Prompt 'Input Username to copy to'
 Get-ADUser -Identity $from -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members $To
